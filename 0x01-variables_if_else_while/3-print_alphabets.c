2de2d0fa918dfcdf24ed8568f2ba95035c06898f
72119sisay <sisayfikru69@gmail.com>
2022-09-09 05:45:24 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Seccess/correct)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 
@@ -11,7 +11,7 @@ char lower_case;
 char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-}
+{
 putchar(lower_case);
 }
 
