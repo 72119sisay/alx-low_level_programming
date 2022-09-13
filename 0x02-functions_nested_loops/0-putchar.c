9aa307ef6cbe16e4c32c17733973090935fb9990
72119sisay <sisayfikru69@gmail.com>
2022-09-13 09:48:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: 0 Success
  */
 int main(void)
-{ 
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -16,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	      return (0);
+		return (0);
 }
