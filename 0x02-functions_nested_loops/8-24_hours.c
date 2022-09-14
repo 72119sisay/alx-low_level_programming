6549fd9666c5e871baee6591500815279444fc0a
72119sisay <sisayfikru69@gmail.com>
2022-09-13 18:53:38 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,16 +15,22 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i >= 10)
+			else if (i > 10)
 			{
 				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
+				_putchar((i % 10) +'0');
 			}
-			if (j <= 10)
+			if (j < 10)
 			{
 				_putchar(':');
 				_putchar('0');
 				_putchar(j + '0');
+			}
+			else if (j >= 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
 		}
