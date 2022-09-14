f89bb6ddebf65be8af4a9039f167a597a31754f7
72119sisay <sisayfikru69@gmail.com>
2022-09-13 17:19:35 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
-
 {
-int i;
-char c;
+	int i;
+	char c;
 
-for (i = 0; i < 10; i++)
-{
-for (c = 'a'; c <= 'z'; c++)
-
-_putchar(c);
-_putchar('\n');
-}
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+        	{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
