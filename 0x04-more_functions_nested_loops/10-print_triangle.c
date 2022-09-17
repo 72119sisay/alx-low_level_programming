ef85dc3e0b9f3875e7c05e5eff1bd7d7978fc625
72119sisay <sisayfikru69@gmail.com>
2022-09-16 17:21:46 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@
 void print_triangle(int size)
 {
 	int inc1, inc2;
-	
+
 	if (size > 0)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
@@ -18,17 +18,17 @@ void print_triangle(int size)
 			{
 				putchar(' ');
 			}
-			
+
 			for (inc2 = 0; inc2 < inc1; inc2++)
 			{
 				putchar('0');
 			}
-			
+
 			if (inc1 == size)
 			{
 				continue;
 			}
-			
+
 			putchar('\n')
 		}
 	}
