539a8b05cd2bee4a0143869cfafb6600c1d5cc12
72119sisay <sisayfikru69@gmail.com>
2022-09-19 16:31:51 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * print_rev - a function that prints a reverse string
- *
- * @s: this is the input string
+*print_rev - a function that prints a reverse string
+*@s: this is the input string
+*Return: Nothing
  */
 void print_rev(char *s)
 {
