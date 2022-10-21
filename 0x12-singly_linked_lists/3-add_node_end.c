26fe935f7545832c6c6eb85e62c6c1c8761c7fed
72119sisay <sisayfikru69@gmail.com>
2022-10-20 18:58:19 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,15 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
+ * add_node - adds a new node at the end of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
  *
  * Return: address of the head.
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node, *current_node;
 	size_t n;
