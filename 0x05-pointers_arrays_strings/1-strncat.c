814953368a5f815d8ea80b1f3b95ce5ce38df8c6
72119sisay <sisayfikru69@gmail.com>
2022-10-25 07:38:45 -0700

    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

--- 0x05-pointers_arrays_strings/1-strncat.c
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * _strncat - concatenate two strings
  * @dest: string
  * @src: string
  * @n: number of elements to concatenate in
+ * 
  * Return: pointer to resulting `dest`
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, c;
